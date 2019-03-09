c4a42f2f51fd36b89369e3374a666837d00d4df1
Adrian Birch <adrian@onesmartdesign.co.uk>
2019-03-09 14:40:44 +0000

    added new files

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <title>My digital playground -- | Adrian Birch | --</title>
+  <meta charset="UTF-8">
+  <meta name="description" content="" />
+  <meta name="keywords" content="" />
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="css/global-styles.css" />
+
+</head>
+
+<body>
+  <div id="page-wrap1">
+
+    <div id="page-wrap2">
+
+      <!-- Header  -->
+      <?php // include_once( 'includes/header-inc.php'); ?>
+
+      <section id="main-content" class="home-page">
+
+        <h1>My digital playground</h1>
+        <p>Web</p>
+
+
+
+
+
+
+      </section>
+
+
+    </div><!-- End page wrap 2 -->
+
+  </div><!-- End page wrap 1 -->
+
+
+  <script>
+
+
+  </script>
+</body>
+
+
+</html>
